4c7b9dd54f1f3ceb4d42c8cadfe06af3740bcbec
Michal <majklfly@gmail.com>
2020-11-18 18:17:59 +0000

    render battle

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { fetchDataForGallery } from "./js/photoGallery";
 
 const Render = async() => {
     console.log("lets get started")
+    // $("#loadingContainer").css("display", "block");
+    // const result = await ajaxGet("currentLocation.php", { key: GEO_API_KEY });
     if (typeof result != "undefined") {
         console.log("running")
         localStorage.setItem("latitude", result.latitude);
