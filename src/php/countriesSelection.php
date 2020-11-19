df22e595139db305e1719c9e333de73d96fa0fa4
majklfly <majklfly@gmail.com>
2020-11-19 12:59:48 +0000

    catching errors

--- src/php/countriesSelection.php
+++ src/php/countriesSelection.php
@@ -5,13 +5,16 @@ $executionStartTime = microtime(true) / 1000;
 $url = "https://restcountries.eu/rest/v2/all";
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_URL,$url);
 
 $result=curl_exec($ch);
 
 curl_close($ch);
+
+echo $result
 
 $decode = json_decode($result,true);	
 
