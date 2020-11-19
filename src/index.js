196d8af0acdeb15a6cf6cb1b37a7597c4224c63c
majklfly <majklfly@gmail.com>
2020-11-19 11:16:56 +0000

    final battle

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ console.log("process", GEO_API_KEY);
 const Render = async() => {
     console.log("lets get started");
     $("#loadingContainer").css("display", "block");
+
     // const result = await ajaxGet("currentLocation.php", { key: GEO_API_KEY });
     if (typeof result != "undefined") {
         console.log("running");
