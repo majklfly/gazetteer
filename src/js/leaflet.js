6b52c75f52b49d9e79d97238829d55fe1d04871a
majklfly <majklfly@gmail.com>
2020-11-19 11:38:16 +0000

    catching errors

--- src/js/leaflet.js
+++ src/js/leaflet.js
@@ -44,28 +44,27 @@ export const leafletmap = async() => {
         });
         localStorage.setItem("latitude", capitalCity.latitude);
         localStorage.setItem("longitude", capitalCity.longitude);
+        var popup = L.popup()
+            .setLatLng([latitude, longitude])
+            .setContent(
+                "<object id='iframePopup' data='src/html/popupContent.html' style='border:none' width='100%' height='100%'></object>"
+            );
+
+        var greenIcon = L.icon({
+            iconUrl: "src/img/leaf-green.png",
+            shadowUrl: "src/img/leaf-shadow.png",
+
+            iconSize: [19, 47], // size of the icon
+            shadowSize: [25, 32], // size of the shadow
+            iconAnchor: [11, 47], // point of the icon which will correspond to marker's location
+            shadowAnchor: [2, 31], // the same for the shadow
+            popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+        });
+
+        L.marker([capitalCity.latitude, capitalCity.longitude], { icon: greenIcon })
+            .addTo(map)
+            .bindPopup(popup);
     } catch (e) {
         console.log("capitalCityError", e);
     }
-
-    var popup = L.popup()
-        .setLatLng([latitude, longitude])
-        .setContent(
-            "<object id='iframePopup' data='src/html/popupContent.html' style='border:none' width='100%' height='100%'></object>"
-        );
-
-    var greenIcon = L.icon({
-        iconUrl: "src/img/leaf-green.png",
-        shadowUrl: "src/img/leaf-shadow.png",
-
-        iconSize: [19, 47], // size of the icon
-        shadowSize: [25, 32], // size of the shadow
-        iconAnchor: [11, 47], // point of the icon which will correspond to marker's location
-        shadowAnchor: [2, 31], // the same for the shadow
-        popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
-    });
-
-    L.marker([capitalCity.latitude, capitalCity.longitude], { icon: greenIcon })
-        .addTo(map)
-        .bindPopup(popup);
 };
