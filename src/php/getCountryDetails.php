b28825e53477ecea35a4ed030669a32a334a1639
majklfly <majklfly@gmail.com>
2020-11-19 22:53:27 +0000

    getting there

--- src/php/getCountryDetails.php
+++ src/php/getCountryDetails.php
@@ -1,7 +1,5 @@
 <?php
 
-$executionStartTime = microtime(true) / 1000;
-
 $url = "https://restcountries.eu/rest/v2/alpha/" . $_REQUEST['countryCode'];
 
 $ch = curl_init();
@@ -15,12 +13,6 @@ curl_close($ch);
 
 $decode = json_decode($result,true);	
 
-$output['status']['code'] = "200";
-$output['status']['name'] = "ok";
-$output['status']['description'] = "mission saved";
-$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
 $output['data'] = $decode;
-
-header('Content-Type: application/json; charset=UTF-8');
 
 echo json_encode($output); 
