f21cb4a6dbdeaf0d245ab314470a5721193758f0
majklfly <majklfly@gmail.com>
2020-11-19 11:23:31 +0000

    final battle

--- src/js/countriesSelection.js
+++ src/js/countriesSelection.js
@@ -3,10 +3,14 @@ import { ajaxGet } from "./utils";
 
 // fetch data from countries api and append the code and name to the select option
 export const countriesSelection = async() => {
-    const list = await ajaxGet("countriesSelection.php");
-    list.map((item) => {
-        $("#countries").append(
-            `<option value=${item.countryCode}>${item.countryName}</option>`
-        );
-    });
+    try {
+        const list = await ajaxGet("countriesSelection.php");
+        list.map((item) => {
+            $("#countries").append(
+                `<option value=${item.countryCode}>${item.countryName}</option>`
+            );
+        });
+    } catch (e) {
+        console.log(e);
+    }
 };
